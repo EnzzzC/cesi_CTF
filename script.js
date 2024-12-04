127b9fa32168f30a032a2bedf10369633eb1b1fe
EnzzzC <enzocolonna52@gmail.com>
2024-12-04 16:46:36 +0100

    niveau 2

--- script.js
+++ script.js
@@ -22,4 +22,18 @@ setTimeout(function() {
 }, 5000);
 localStorage.setItem("password1","bienvu321")
 let actions = ["vérifier", "exécuter", "démarrer", "stopper"];
-let randomAction = actions[Math.floor(Math.random() * actions.length)];    
+let randomAction = actions[Math.floor(Math.random() * actions.length)];
+
+const morseCode = {
+    A: ".-", B: "-...", C: "-.-.", D: "-..", E: ".", F: "..-.", G: "--.",
+    H: "....", I: "..", J: ".---", K: "-.-", L: ".-..", M: "--", N: "-.",
+    O: "---", P: ".--.", Q: "--.-", R: ".-.", S: "...", T: "-", U: "..-",
+    V: "...-", W: ".--", X: "-..-", Y: "-.--", Z: "--..",
+    1: ".----", 2: "..---", 3: "...--", 4: "....-", 5: ".....",
+    6: "-....", 7: "--...", 8: "---..", 9: "----.", 0: "-----",
+    " ": "/"
+};
+
+function fhugprgv(text) {
+    return text.toUpperCase().split('').map(char => morseCode[char] || '?').join(' ');
+}
